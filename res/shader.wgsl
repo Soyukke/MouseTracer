957afbbafbd3b94ee2d9082872f6473307c6e9cf
kyosuke.sato <absoexrd@gmail.com>
2023-02-21 16:22:31 +0900

    use texture

--- res/shader.wgsl
+++ res/shader.wgsl
@@ -1,11 +1,50 @@
+struct Globals {
+    mvp: mat4x4<f32>,
+    size: vec2<f32>,
+    _pad0: u32,
+    _pad1: u32,
+};
+
+struct Locals {
+    position: vec2<f32>,
+    velocity: vec2<f32>,
+    color: u32,
+    _pad0: u32,
+    _pad1: u32,
+    _pad2: u32,
+};
+
+@group(0)
+@binding(0)
+var<uniform> globals: Globals;
+
+@group(1)
+@binding(0)
+var<uniform> locals: Locals;
+
+struct VertexOutput {
+    @builtin(position) position: vec4<f32>,
+    @location(0) tex_coords: vec2<f32>,
+    @location(1) color: vec4<f32>,
+};
+
 @vertex
-fn vs_main(@builtin(vertex_index) in_vertex_index: u32) -> @builtin(position) vec4<f32> {
-    let x = f32(i32(in_vertex_index) - 1);
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1);
-    return vec4<f32>(x, y, 0.0, 1.0);
+fn vs_main(@builtin(vertex_index) vi: u32) -> VertexOutput {
+    let tc = vec2<f32>(f32(vi & 1u), 0.5 * f32(vi & 2u));
+    let offset = vec2<f32>(tc.x * globals.size.x, tc.y * globals.size.y);
+    let pos = globals.mvp * vec4<f32>(locals.position + offset, 0.0, 1.0);
+    let color = vec4<f32>(255.0, 255.0, 255.0, 255.0) / 255.0;
+    return VertexOutput(pos, tc, color);
 }
 
+@group(0)
+@binding(1)
+var tex: texture_2d<f32>;
+@group(0)
+@binding(2)
+var sam: sampler;
+
 @fragment
-fn fs_main() -> @location(0) vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn fs_main(vertex: VertexOutput) -> @location(0) vec4<f32> {
+    return vertex.color * textureSampleLevel(tex, sam, vertex.tex_coords, 0.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-use std::borrow::Cow;
+use bytemuck::{Pod, Zeroable};
+use nanorand::{Rng, WyRand};
+use std::{borrow::Cow, mem};
 use enigo::Enigo;
 use winit::{
     event::{Event, WindowEvent},
@@ -6,8 +8,32 @@ use winit::{
     window::Window, dpi::{LogicalSize, LogicalPosition},
 };
 use image::io::Reader as ImageReader;
+use wgpu::util::DeviceExt;
 
-fn getTexture(device: wgpu::Device, queue: &wgpu::Queue) {
+const WINDOW_WIDTH: f32  = 75.0;
+
+#[repr(C)]
+#[derive(Clone, Copy, Pod, Zeroable)]
+struct Globals {
+    mvp: [[f32; 4]; 4],
+    size: [f32; 2],
+    pad: [f32; 2],
+}
+
+/**
+ * 座標(x, y): [f32, 2]
+ * 
+ */
+#[repr(C, align(256))]
+#[derive(Clone, Copy, Zeroable)]
+struct Locals {
+    position: [f32; 2],
+    velocity: [f32; 2],
+    color: u32,
+    _pad: u32,
+}
+
+fn get_texture(device: &wgpu::Device, queue: &wgpu::Queue) -> wgpu::Texture {
         let texture = {
             let img_data = include_bytes!("../assets/image.png");
             let dyimg = image::load_from_memory(img_data).unwrap();
@@ -15,10 +41,11 @@ fn getTexture(device: wgpu::Device, queue: &wgpu::Queue) {
             //let mut reader = decoder.read_info().unwrap();
             //let mut buf = vec![0; reader.output_buffer_size()];
             //let info = reader.next_frame(&mut buf).unwrap();
+            let dyimg2 = dyimg.into_rgba8();
 
             let size = wgpu::Extent3d {
-                width: dyimg.width(),
-                height: dyimg.height(),
+                width: dyimg2.width(),
+                height: dyimg2.height(),
                 depth_or_array_layers: 1,
             };
             let texture = device.create_texture(&wgpu::TextureDescriptor {
@@ -32,19 +59,17 @@ fn getTexture(device: wgpu::Device, queue: &wgpu::Queue) {
             });
             queue.write_texture(
                 texture.as_image_copy(),
-                dyimg.as_bytes(),
+                &dyimg2.into_raw(),
                 wgpu::ImageDataLayout {
                     offset: 0,
-                    bytes_per_row: std::num::NonZeroU32::new(dyimg.width() * 4),
+                    bytes_per_row: std::num::NonZeroU32::new(size.width * 4),
                     rows_per_image: None,
                 },
                 size,
             );
             texture
         };
-        let view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        texture;
+        texture
 }
 
 // 参考
@@ -87,15 +112,155 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
         source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(include_str!("../res/shader.wgsl"))),
     });
 
-    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+    let global_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[
+                wgpu::BindGroupLayoutEntry {
+                    binding: 0,
+                    visibility: wgpu::ShaderStages::VERTEX,
+                    ty: wgpu::BindingType::Buffer {
+                        ty: wgpu::BufferBindingType::Uniform,
+                        has_dynamic_offset: false,
+                        min_binding_size: wgpu::BufferSize::new(mem::size_of::<Globals>() as _),
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 1,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Texture {
+                        sample_type: wgpu::TextureSampleType::Float { filterable: true },
+                        view_dimension: wgpu::TextureViewDimension::D2,
+                        multisampled: false,
+                    },
+                    count: None,
+                },
+                wgpu::BindGroupLayoutEntry {
+                    binding: 2,
+                    visibility: wgpu::ShaderStages::FRAGMENT,
+                    ty: wgpu::BindingType::Sampler(wgpu::SamplerBindingType::Filtering),
+                    count: None,
+                },
+                ],
+                label: None,
+        });
+
+
+
+    let local_bind_group_layout =
+        device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+            entries: &[wgpu::BindGroupLayoutEntry {
+                binding: 0,
+                visibility: wgpu::ShaderStages::VERTEX,
+                ty: wgpu::BindingType::Buffer {
+                    ty: wgpu::BufferBindingType::Uniform,
+                    has_dynamic_offset: true,
+                    min_binding_size: wgpu::BufferSize::new(mem::size_of::<Locals>() as _),
+                },
+                count: None,
+            }],
+            label: None,
+        });
+
+    let view = get_texture(&device, &queue).create_view(&wgpu::TextureViewDescriptor::default());
+
+    let sampler = device.create_sampler(&wgpu::SamplerDescriptor {
         label: None,
-        bind_group_layouts: &[],
-        push_constant_ranges: &[],
+        address_mode_u: wgpu::AddressMode::ClampToEdge,
+        address_mode_v: wgpu::AddressMode::ClampToEdge,
+        address_mode_w: wgpu::AddressMode::ClampToEdge,
+        mag_filter: wgpu::FilterMode::Linear,
+        min_filter: wgpu::FilterMode::Nearest,
+        mipmap_filter: wgpu::FilterMode::Nearest,
+        ..Default::default()
     });
 
     let swapchain_capabilities = surface.get_supported_formats(&adapter);
     let swapchain_format = swapchain_capabilities[0];
     println!("swapchain_format: {:?}", swapchain_format);
+
+    let mut config = wgpu::SurfaceConfiguration {
+        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+        format: swapchain_format,
+        width: size.width,
+        height: size.height,
+        present_mode: wgpu::PresentMode::Fifo,
+        // alpha_mode: swapchain_capabilities.alpha_modes[0],
+        alpha_mode: wgpu::CompositeAlphaMode::PostMultiplied,
+    };
+
+    let globals = Globals {
+        mvp: glam::Mat4::orthographic_rh(
+                 0.0,
+                 WINDOW_WIDTH,
+                 0.0,
+                 WINDOW_WIDTH,
+                 -1.0,
+                 1.0,
+                 )
+            .to_cols_array_2d(),
+            size: [0.15*256.0; 2],
+            pad: [0.0; 2],
+    };
+
+    let global_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+        label: Some("global"),
+        contents: bytemuck::bytes_of(&globals),
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
+    });
+
+
+    let uniform_alignment =
+        device.limits().min_uniform_buffer_offset_alignment as wgpu::BufferAddress;
+    let local_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+        label: Some("local"),
+        size: (1 as wgpu::BufferAddress) * uniform_alignment,
+        usage: wgpu::BufferUsages::COPY_DST | wgpu::BufferUsages::UNIFORM,
+        mapped_at_creation: false,
+    });
+
+
+    let global_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &global_bind_group_layout,
+        entries: &[
+            wgpu::BindGroupEntry {
+                binding: 0,
+                resource: global_buffer.as_entire_binding(),
+            },
+            wgpu::BindGroupEntry {
+                binding: 1,
+                resource: wgpu::BindingResource::TextureView(&view),
+            },
+            wgpu::BindGroupEntry {
+                binding: 2,
+                resource: wgpu::BindingResource::Sampler(&sampler),
+            },
+        ],
+        label: None,
+    });
+
+
+    let local_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+        layout: &local_bind_group_layout,
+        entries: &[wgpu::BindGroupEntry {
+            binding: 0,
+            resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                buffer: &local_buffer,
+                offset: 0,
+                size: wgpu::BufferSize::new(mem::size_of::<Locals>() as _),
+            }),
+        }],
+        label: None,
+    });
+
+
+    let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+        label: None,
+        bind_group_layouts: &[&global_bind_group_layout, &local_bind_group_layout],
+        push_constant_ranges: &[],
+    });
+
+
 
     let render_pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
         label: None,
@@ -110,21 +275,16 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
             entry_point: "fs_main",
             targets: &[Some(swapchain_format.into())],
         }),
-        primitive: wgpu::PrimitiveState::default(),
+        // 矩形の4点で描画する
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleStrip,
+            strip_index_format: Some(wgpu::IndexFormat::Uint16),
+            ..wgpu::PrimitiveState::default()
+        },
         depth_stencil: None,
         multisample: wgpu::MultisampleState::default(),
         multiview: None,
     });
-
-    let mut config = wgpu::SurfaceConfiguration {
-        usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-        format: swapchain_format,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Fifo,
-        // alpha_mode: swapchain_capabilities.alpha_modes[0],
-        alpha_mode: wgpu::CompositeAlphaMode::PostMultiplied,
-    };
 
     surface.configure(&device, &config);
 
@@ -133,8 +293,9 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 
         // Mouseポジション
         let cursor_location: (i32, i32) = Enigo::mouse_location();
+        //let p = LogicalPosition{x: cursor_location.0 - (WINDOW_WIDTH/2.0) as i32, y: cursor_location.1 - (WINDOW_WIDTH/2.0) as i32};
         let p = LogicalPosition{x: cursor_location.0, y: cursor_location.1};
-        println!("cursor: {:?}", cursor_location);
+        // println!("cursor: {:?}", cursor_location);
         // Windowポジションをマウスの場所へ変更
         window.set_outer_position(p);
 
@@ -147,8 +308,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 ..
             } => {
                 // Reconfigure the surface with the new size
-                config.width = size.width;
-                config.height = size.height;
+                // Window sizeにする
                 surface.configure(&device, &config);
                 // On macos the window needs to be redrawn manually after resizing
                 window.request_redraw();
@@ -163,6 +323,25 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                 let mut encoder =
                     device.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
                 {
+
+                    // localsに書き込む
+                    let mut rng = WyRand::new_seed(42);
+                    let color = rng.generate::<u32>();
+                    let mut rects: Vec<Locals> = vec![];
+                    let extent = [WINDOW_WIDTH, WINDOW_WIDTH];
+                    rects.push(Locals {
+                        position: [0.0, 0.5 * (extent[1] as f32)],
+                        velocity: [0.0, 0.0],
+                        color,
+                        _pad: 0,
+                    });
+                    let uniform_alignment = device.limits().min_uniform_buffer_offset_alignment;
+                    queue.write_buffer(&local_buffer, 0, unsafe {
+                        std::slice::from_raw_parts(
+                            rects.as_ptr() as *const u8,
+                            rects.len() * uniform_alignment as usize,
+                            )
+                    });
                     let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
                         label: None,
                         color_attachments: &[Some(wgpu::RenderPassColorAttachment {
@@ -177,7 +356,11 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
                         depth_stencil_attachment: None,
                     });
                     rpass.set_pipeline(&render_pipeline);
-                    rpass.draw(0..3, 0..1);
+                    rpass.set_bind_group(0, &global_group, &[]);
+                    let offset =
+                        (0 as wgpu::DynamicOffset) * (uniform_alignment as wgpu::DynamicOffset);
+                    rpass.set_bind_group(1, &local_group, &[offset]);
+                    rpass.draw(0..4, 0..1);
                 }
 
                 queue.submit(Some(encoder.finish()));
@@ -198,7 +381,7 @@ fn main() {
     let mut builder = winit::window::WindowBuilder::new()
                 .with_decorations(false)
                 .with_always_on_top(true)
-                .with_inner_size(LogicalSize::new(25.0, 25.0))
+                .with_inner_size(LogicalSize::new(WINDOW_WIDTH, WINDOW_WIDTH))
                 .with_transparent(true);
     builder = builder.with_title("hello-triangle");
     #[cfg(windows_OFF)] // TODO
